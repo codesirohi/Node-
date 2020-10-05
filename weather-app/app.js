6e6b5e45c8ebc6cbf6460c313935af8269a3af14
Shubham Singh <38425721+codesirohi@users.noreply.github.com>
2020-10-05 18:21:22 +0530

    Update app.js

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,6 +1,6 @@
 const request = require('request')
 
-const url = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=dba78ec59cfe873a0062f443838a9901'
+const url = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=dba78ec59cfexxxa0062f443838a9901'
 
 request({ url: url, json: true }, (error, response) =>{
     console.log(response.body.main)
